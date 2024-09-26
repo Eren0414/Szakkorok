603eff2ed401b5bd217803487740829c50274f29
Eren0414 <kritof.jager.2006@gmail.com>
2024-09-26 19:22:34 +0200

    szakkorok kesz

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,43 +3,49 @@
     <h1>Szakkörök</h1>
     <div class="split">
       <div class="left">
-        <ul class="list-group">
-          <li 
-            v-for="tanulo in tanulok" 
-            :key="tanulo.id" 
-            class="list-group-item" 
-            @click="selectTanulo(tanulo)"
-          >
-            {{ tanulo.nev }} ({{ tanulo.osztaly }})
-          </li>
-        </ul>
-        <div class="mt-3">
-          <select v-model="kivalasztottSzakkor" id="szakkorValaszto">
-            <option v-for="szakkor in szakkorok" :key="szakkor.id" :value="szakkor.nev">
-              {{ szakkor.nev }}
-            </option>
-          </select>
-          <button @click="checkSzakkor()">Ellenőrzés</button>
-        </div>
+        <table class="table col-6">
+          <thead>
+            <tr>
+              <th scope="col">Diákok</th>
+              <th scope="col">Osztályok</th>
+              <th scope="col">Szakkörök</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="tanulo in tanulok" :key="tanulo.id">
+              <td>{{ tanulo.nev }}</td>
+              <td>{{ tanulo.osztaly }}</td>
+              <td>
+                <select v-model="tanulo.kivalasztottSzakkor" @change="addTanuloToSzakkor(tanulo)">
+                  <option v-for="szakkor in szakkorok" :key="szakkor.id" :value="szakkor.nev">
+                    {{ szakkor.nev }}
+                  </option>
+                </select>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
       <div class="right">
-        <div class="card" v-if="selectedTanulo">
+        <div class="card" v-for="(tanulok, szakkor) in szakkorFeladatok" :key="szakkor">
           <div class="card-body">
-            <h5 class="card-title">{{ selectedTanulo.nev }}</h5>
-            <p class="card-text">
-              <span v-if="selectedTanulo.jelenlegiSzakkor">{{ selectedTanulo.jelenlegiSzakkor }}</span>
-              <span v-else>Nem jár szakkörre</span>
+            <h5 class="card-title">{{ szakkor }}</h5>
+            <p v-if="tanulok.length">
+              Tanulók: {{ tanulok.join(', ') }}
             </p>
-            <p class="card-text" v-if="szakkorIsInTanulo()">
-              Jelenlegi szakkör: {{ kivalasztottSzakkor }}
-            </p>
-            <p class="card-text" v-else>
-              Az adott tanuló nem jár a szakkörre.
-            </p>
+            <p v-else>Nincs tanuló ebben a szakkörben</p>
           </div>
         </div>
-        <p v-else>Kérlek válassz ki egy tanulót!</p>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Még nincs szakköre</h5>
+            <p v-if="noSzakkorTanulok.length">
+              Tanulók: {{ noSzakkorTanulok.join(', ') }}
+            </p>
+            <p v-else>Minden tanulónak van szakköre.</p>
+          </div>
+        </div>
       </div>
     </div>
   </main>
@@ -50,48 +56,66 @@ export default {
   data() {
     return {
       tanulok: [
-        { id: 1, nev: "Jacob Thornton", osztaly: "9.A", jelenlegiSzakkor: "Foci" },
-        { id: 2, nev: "Mark Otto", osztaly: "10.B", jelenlegiSzakkor: "Zene" },
-        { id: 3, nev: "Larry Bird", osztaly: "11.C", jelenlegiSzakkor: "Programozás" },
-        { id: 4, nev: "Anna Smith", osztaly: "9.A", jelenlegiSzakkor: "" },
-        { id: 5, nev: "David Johnson", osztaly: "10.B", jelenlegiSzakkor: "" },
-        { id: 6, nev: "James Brown", osztaly: "11.C", jelenlegiSzakkor: "" },
-        { id: 7, nev: "Emily Davis", osztaly: "9.A", jelenlegiSzakkor: "" },
-        { id: 8, nev: "Michael Miller", osztaly: "10.B", jelenlegiSzakkor: "" },
-        { id: 9, nev: "Sarah Wilson", osztaly: "11.C", jelenlegiSzakkor: "" },
-        { id: 10, nev: "Chris Lee", osztaly: "9.A", jelenlegiSzakkor: "" },
-        { id: 11, nev: "Emma Moore", osztaly: "10.B", jelenlegiSzakkor: "" },
-        { id: 12, nev: "John Anderson", osztaly: "11.C", jelenlegiSzakkor: "" },
-        { id: 13, nev: "Olivia Martinez", osztaly: "9.A", jelenlegiSzakkor: "" },
-        { id: 14, nev: "Sophia Thompson", osztaly: "10.B", jelenlegiSzakkor: "" },
-        { id: 15, nev: "Mason White", osztaly: "11.C", jelenlegiSzakkor: "" }
+        { id: 1, nev: "Kovács Péter", osztaly: "9.A", kivalasztottSzakkor: "" },
+        { id: 2, nev: "Szabó Anna", osztaly: "10.B", kivalasztottSzakkor: "" },
+        { id: 3, nev: "Nagy Gábor", osztaly: "11.C", kivalasztottSzakkor: "" },
+        { id: 4, nev: "Tóth Katalin", osztaly: "9.A", kivalasztottSzakkor: "" },
+        { id: 5, nev: "Varga Tamás", osztaly: "10.B", kivalasztottSzakkor: "" },
+        { id: 6, nev: "Horváth Zoltán", osztaly: "11.C", kivalasztottSzakkor: "" },
+        { id: 7, nev: "Balogh Eszter", osztaly: "10.B", kivalasztottSzakkor: "" },
+        { id: 8, nev: "Molnár Ádám", osztaly: "9.A", kivalasztottSzakkor: "" },
+        { id: 9, nev: "Kocsis László", osztaly: "10.B", kivalasztottSzakkor: "" },
+        { id: 10, nev: "Papp Júlia", osztaly: "11.C", kivalasztottSzakkor: "" },
+        { id: 11, nev: "Kerekes Ferenc", osztaly: "10.B", kivalasztottSzakkor: "" },
+        { id: 12, nev: "Farkas Anikó", osztaly: "11.C", kivalasztottSzakkor: "" },
+        { id: 13, nev: "Kiss István", osztaly: "9.A", kivalasztottSzakkor: "" },
+        { id: 14, nev: "Sipos Emese", osztaly: "11.C", kivalasztottSzakkor: "" },
+        { id: 15, nev: "Rácz Balázs", osztaly: "10.B", kivalasztottSzakkor: "" }
       ],
       szakkorok: [
         { id: 1, nev: "Foci" },
         { id: 2, nev: "Zene" },
         { id: 3, nev: "Programozás" }
       ],
-      kivalasztottSzakkor: "", 
-      selectedTanulo: null,
+      szakkorFeladatok: {
+        Foci: [],
+        Zene: [],
+        Programozás: []
+      }
     };
   },
+  computed: {
+    noSzakkorTanulok() {
+      return this.tanulok
+        .filter(tanulo => !tanulo.kivalasztottSzakkor)
+        .map(tanulo => tanulo.nev);
+    }
+  },
   methods: {
-    selectTanulo(tanulo) {
-      this.selectedTanulo = tanulo;
-    },
-    checkSzakkor() {
-      if (this.selectedTanulo) {
-        this.selectedTanulo.jelenlegiSzakkor = this.kivalasztottSzakkor;
+    addTanuloToSzakkor(tanulo) {
+      // Remove the student from all szakkorok
+      for (let szakkor in this.szakkorFeladatok) {
+        const index = this.szakkorFeladatok[szakkor].indexOf(tanulo.nev);
+        if (index !== -1) {
+          this.szakkorFeladatok[szakkor].splice(index, 1);
+        }
       }
-    },
-    szakkorIsInTanulo() {
-      return this.selectedTanulo.jelenlegiSzakkor === this.kivalasztottSzakkor;
+
+      // If a szakkor is selected, add the student to the chosen szakkor
+      if (tanulo.kivalasztottSzakkor) {
+        this.szakkorFeladatok[tanulo.kivalasztottSzakkor].push(tanulo.nev);
+      }
     }
   }
 };
 </script>
 
+
 <style scoped>
+.table {
+  max-width: 600px;
+}
+
 h1 {
   text-align: center;
   border: 4px black solid;
@@ -104,6 +128,8 @@ h1 {
 .left {
   flex: 40%;
   margin-right: 20px;
+  border-right: 2px solid black; 
+  padding-right: 10px; 
 }
 
 .right {
@@ -112,33 +138,11 @@ h1 {
 
 .card {
   margin-bottom: 10px;
-}
-
-button {
-  background-color: #4CAF50;
-  color: white;
-  padding: 10px;
-  border: none;
-  cursor: pointer;
-  margin-top: 10px;
-}
-
-button:hover {
-  background-color: #45a049;
-}
-
-.list-group {
-  max-height: 300px;
-  overflow-y: auto;
-}
-
-.card-title {
-  margin-bottom: 10px;
+  width: 450px;
 }
 
 select {
   padding: 5px;
-  margin-top: 10px;
-  width: 100%;
+  margin-top: 5px;
 }
 </style>
