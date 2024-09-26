99f6211b7bc35851de6ec15fab00ea598afac403
Eren0414 <kritof.jager.2006@gmail.com>
2024-09-26 10:25:52 +0200

    vue kesz

---
+++ Szakkorok/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+ <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
